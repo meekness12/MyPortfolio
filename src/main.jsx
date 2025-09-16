74fb527d7c09a4e8da1612c102f031b9f98b0779
Meekness12 <meeknessbon@gmail.com>
2025-09-16 11:58:05 +0200

    fixing contact  form submit color error

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App'; // Assuming your App.js file is in the same directory
-import './index.css'; // Make sure your Tailwind CSS output is imported here
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App"; 
+import "./index.css"; 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// 👇 Add Chatbase config here
+window.chatbaseConfig = {
+  chatbotId: import.meta.env.VITE_CHATBASE_BOT_ID, // pulled from .env
+};
+
+// Dynamically load Chatbase script
+const script = document.createElement("script");
+script.src = "https://www.chatbase.co/embed.min.js";
+script.defer = true;
+document.body.appendChild(script);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
